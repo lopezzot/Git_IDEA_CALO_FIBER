f1cb852efe83c7a257c2a9ed0dd9daaba9d00cfa
Lorenzo Pezzotti <lorenzo.pezzotti@cern.ch>
2021-02-10 11:40:13 +0100

    adding struct

--- DRC4pifibers/DRC4pigeometry/Dream/B4/B4a/include/B4aEventAction.hh
+++ DRC4pifibers/DRC4pigeometry/Dream/B4/B4a/include/B4aEventAction.hh
@@ -1,18 +1,18 @@
 //
 // ********************************************************************
-// * License and Disclaimer                                           *
+// * license and disclaimer                                           *
 // *                                                                  *
-// * The  Geant4 software  is  copyright of the Copyright Holders  of *
-// * the Geant4 Collaboration.  It is provided  under  the terms  and *
-// * conditions of the Geant4 Software License,  included in the file *
-// * LICENSE and available at  http://cern.ch/geant4/license .  These *
+// * the  geant4 software  is  copyright of the copyright holders  of *
+// * the geant4 collaboration.  it is provided  under  the terms  and *
+// * conditions of the geant4 software license,  included in the file *
+// * license and available at  http://cern.ch/geant4/license .  these *
 // * include a list of copyright holders.                             *
 // *                                                                  *
-// * Neither the authors of this software system, nor their employing *
+// * neither the authors of this software system, nor their employing *
 // * institutes,nor the agencies providing financial support for this *
 // * work  make  any representation or  warranty, express or implied, *
 // * regarding  this  software system or assume any liability for its *
-// * use.  Please see the license in the file  LICENSE  and URL above *
+// * use.  please see the license in the file  license  and url above *
 // * for the full disclaimer and the limitation of liability.         *
 // *                                                                  *
 // * This  code  implementation is the result of  the  scientific and *
@@ -50,18 +50,16 @@ class B4aEventAction : public G4UserEventAction
     virtual ~B4aEventAction();
 
     virtual void  BeginOfEventAction(const G4Event* event);
-    virtual void    EndOfEventAction(const G4Event* event);
+    virtual void  EndOfEventAction(const G4Event* event);
     
-    void Addneutrinoleakage(G4double de); //add energy of neutrinos in the ball containing the calorimeter
-    void Addleakage(G4double de); //add energy of all particles that are not neutrinos (or anti_neutrinos) in the ball containing the calorimeter
-    void Addem(G4double de);  //Add em component
-    void AddScin(G4double de);//Add energy in scintillating fibers
-    void AddCher(G4double de);//Add energy in Cherenkov fibers
-    void AddCherenkov();//Add cherenkov photoelectron
-    //void AddScintillation();
-    void Addenergy(G4double de);//Add all energy deposited
-    //void AddEnergyfibre(G4double de, G4int number);//Add energy in copy number fiber
-    //void AddSignalfibre(G4int number);
+    //to save double
+    void Addneutrinoleakage(G4double de);          //add energy of neutrinos in the leakage counter containing the calorimeter
+    void Addleakage(G4double de);                  //add energy of all particles that are not neutrinos (or anti_neutrinos) in the leakage counter
+    void Addem(G4double de);                       //add em component
+    void AddScin(G4double de);                     //add energy in scintillating fibers
+    void AddCher(G4double de);                     //add energy in Cherenkov fibers
+    void AddCherenkov();                           //add cherenkov photoelectron
+    void Addenergy(G4double de);                   //add energy deposited in the calorimeter
     void SavePrimaryParticle(G4String name);
     void SavePrimaryEnergy(G4double primaryparticleenergy);
 
@@ -76,16 +74,33 @@ class B4aEventAction : public G4UserEventAction
     std::vector<G4double>& GetVectorL_loop() {return VectorL_loop;}
 
     //to fill vectors
-    void AddVectorScinEnergyR(G4double de, G4int tower, G4int slice); //fill vector of scintillating fibers with energy deposition
+    void AddVectorScinEnergyR(G4double de, G4int tower, G4int slice); //fill vector of scintillating fibers with signal (tower by tower)
     void AddVectorScinEnergyL(G4double de, G4int tower, G4int slice); //fill vector left side
-    void AddVectorCherPER(G4int c_signal, G4int tower, G4int slice);//fill vector of cherenkov fibers with chernekov photoelectrons
-    void AddVectorCherPEL(G4int c_signal, G4int tower, G4int slice);
-    void AddVectorR(G4double de, G4int tower, G4int slice);
-    void AddVectorL(G4double de, G4int tower, G4int slice);
+    void AddVectorCherPER(G4int c_signal, G4int tower, G4int slice);  //fill vector of cherenkov fibers with chernekov signal (tower by tower) 
+    void AddVectorCherPEL(G4int c_signal, G4int tower, G4int slice);  //fill vector left side
+    void AddVectorR(G4double de, G4int tower, G4int slice);           //fill vector of energy deposited in tower (tower by tower)
+    void AddVectorL(G4double de, G4int tower, G4int slice);           //fill vector left side
     void AddVectorR_loop(G4double de, G4int tower, G4int slice);
     void AddVectorL_loop(G4double de, G4int tower, G4int slice);
 
-    
+    //define fiber struct: contain info of fiber integrated signal and fiber location, include methods to fill it hit-by-hit
+    struct Fiber{
+      int ID, Type, Slice, Tower;           //fiber ID, type (S or C), slice, tower
+      G4double E;                           //number of p.e. in fiber 
+      G4ThreeVector Pos;                    //inner tip position (X,Y,Z) (mm)
+      std::vector<float> phtimes;                //vector of float, each float is a single photon time of arrival 
+      
+      void addfiber(Fiber *f1){             //function to add single hit to fiber integrated signal (single hit passed by reference)
+	E = E+f1->E;                        //add hit photons
+	phtimes.insert(phtimes.end(), f1->phtimes.begin(), f1->phtimes.end()); //append photons time of arriva
+      
+      };	
+      
+      void orderphtimes(){ //function to order photon times from first to last
+	sort(phtimes.begin(), phtimes.end());
+      };
+    };
+
     typedef struct FiberInfo {
         G4double F_ID, F_E, F_X, F_Y, F_Z; //fiber saturated energy
         G4int F_Type, F_slice, F_tower; //C==0 S ==1;
@@ -93,59 +108,45 @@ class B4aEventAction : public G4UserEventAction
     
     void WriteFiber_Info(G4double FID, G4double FE, G4int FType, G4ThreeVector Fpos, G4int slice, G4int tower);
     
-    typedef struct TrackingInfo {
-        G4double T_ID, T_X, T_Y, T_Z, T_Ek;
-        G4String T_Name;
-    } Tracking_Info;
+    void PrepareForRun();
     
-    void WriteTracking_Info(G4double T_ID, G4ThreeVector Tpos, G4String Name, G4double Ek);
-  void PrepareForRun();
-    
-  private:
-    G4int cont;
+private:
+    std::vector<Fiber> Fibers;           //vector of Fibers
+    std::vector<int> FiberIDs;           //vector of Fibers IDs
     Fiber_Info Fiber_Hits[1000000];
-    Tracking_Info Tracking_Hits[200];
-    G4double  Energyem; //Energy of em component
-    G4double  EnergyScin; //Energy in scintillating fibers
-    G4double  EnergyCher; //Energy in Cherenkov fibers
-    G4int     NofCherenkovDetected; //Number of Cherenkov photons detected (in cherenkov fibers)
-    //G4int     NofScintillationDetected;//Number of Scintillating photons detected (in scintillating fibers)
-    G4double  EnergyTot;//Total energy deposited (does not count invisibile energy)
-    //G4double  Signalfibre[64];//Signal in 64 single module fibers, to be used with AddEnergyfibre
-    G4String PrimaryParticleName; //Name of primary particle
-    G4double PrimaryParticleEnergy;//Primary particle energy
-    G4double neutrinoleakage; //leakage neutrino
-    G4double leakage; //leakage non neutrino
+    G4double  Energyem;             //energy of em component
+    G4double  EnergyScin;           //energy in scintillating fibers
+    G4double  EnergyCher;           //energy in Cherenkov fibers
+    G4int     NofCherenkovDetected; //number of Cherenkov photons detected (in cherenkov fibers)
+    G4double  EnergyTot;            //total energy deposited (does not count invisibile energy)
+    G4String PrimaryParticleName;   //name of primary particle
+    G4double PrimaryParticleEnergy; //primary particle energy
+    G4double neutrinoleakage;       //leakage neutrino
+    G4double leakage;               //leakage non neutrino
 
     std::vector<G4double> VectorR_loop;
     std::vector<G4double> VectorL_loop;
-
-    std::vector<G4double> VectorSignalsR;//Vector filled with scintillating fibers energy deposits
-    std::vector<G4double> VectorSignalsL;//vector filled for left side
-    std::vector<G4double> VectorSignalsCherR;//Vector filled with Cherenkov fibers Cherenkov photoelectrons
-    std::vector<G4double> VectorSignalsCherL;//vector filled for left side
+    std::vector<G4double> VectorSignalsR;     //vector filled with scintillating signal tower-by-tower
+    std::vector<G4double> VectorSignalsL;     //vector filled for right side
+    std::vector<G4double> VectorSignalsCherR; //Vector filled with Cherenkov signal tower-by-tower
+    std::vector<G4double> VectorSignalsCherL; //vector filled for left side
+    std::vector<G4double> VectorR;            //vector with energy deposited in towers 
+    std::vector<G4double> VectorL;            //vector fille for left side
     
-    std::vector<G4double> VectorR; //vector with energy deposited in towers right
-    std::vector<G4double> VectorL;
-  edm4hep::SimCalorimeterHitCollection * s_caloHits;
-  edm4hep::SimCalorimeterHitCollection * c_caloHits;
-  edm4hep::SimCalorimeterHitCollection * aux_infoHits;
+    //define edm4hep calo hits + auxiliary info
+    edm4hep::SimCalorimeterHitCollection * s_caloHits;
+    edm4hep::SimCalorimeterHitCollection * c_caloHits;
+    edm4hep::SimCalorimeterHitCollection * aux_infoHits;
 };
 
 // inline functions
-inline void B4aEventAction::Addneutrinoleakage(G4double de){
-    neutrinoleakage += de;
-}
+inline void B4aEventAction::Addneutrinoleakage(G4double de){neutrinoleakage += de;}
 
-inline void B4aEventAction::Addleakage(G4double de){
-    leakage += de;
-}
+inline void B4aEventAction::Addleakage(G4double de){leakage += de;}
 
-inline void B4aEventAction::AddVectorR(G4double de, G4int tower, G4int slice){
-	VectorR.at(tower+(slice*75)) += de;	
-}
+inline void B4aEventAction::AddVectorR(G4double de, G4int tower, G4int slice){VectorR.at(tower+(slice*75)) += de;}
 
-inline void B4aEventAction::AddVectorL(G4double de, G4int tower, G4int slice){
+inline void B4aEventAction::AddVectorL(G4double de, G4int tower, G4int slice){tower = -1*tower;
 	tower = -1*tower;
 	VectorL.at(tower+(slice*75)) += de;
 }
@@ -173,26 +174,9 @@ inline void B4aEventAction::WriteFiber_Info(G4double FID, G4double FE, G4int FTy
     Fiber_Hits[k].F_tower = tower;
 }
 
-inline void B4aEventAction::WriteTracking_Info(G4double TID, G4ThreeVector Tpos, G4String Name, G4double Ek){
-    int k=0;
-    while (Tracking_Hits[k].T_ID!=0 && Tracking_Hits[k].T_ID!=TID){
-        k++;}
-    Tracking_Hits[k].T_ID = TID;
-    Tracking_Hits[k].T_X = Tpos[0];
-    Tracking_Hits[k].T_Y = Tpos[1];
-    Tracking_Hits[k].T_Z = Tpos[2];
-    Tracking_Hits[k].T_Name = Name;
-    if (Tracking_Hits[k].T_Ek<=0.) {Tracking_Hits[k].T_Ek = Ek;}
-}
+inline void B4aEventAction::SavePrimaryParticle(G4String name){PrimaryParticleName = name;}
 
-
-inline void B4aEventAction::SavePrimaryParticle(G4String name){
-  PrimaryParticleName = name;
-}
-
-inline void B4aEventAction::SavePrimaryEnergy(G4double primaryparticleenergy){
-  PrimaryParticleEnergy = primaryparticleenergy;
-}
+inline void B4aEventAction::SavePrimaryEnergy(G4double primaryparticleenergy){PrimaryParticleEnergy = primaryparticleenergy;}
 
 inline void B4aEventAction::AddVectorScinEnergyR(G4double de, G4int tower, G4int slice) {
     VectorSignalsR.at(tower+(slice*75)) += de;
@@ -212,41 +196,14 @@ inline void B4aEventAction::AddVectorCherPER(G4int c_signal, G4int tower, G4int 
     VectorSignalsCherR.at(tower+(slice*75)) = VectorSignalsCherR.at(tower+(slice*75))+c_signal;
 }
 
-inline void B4aEventAction::Addem(G4double de) {
-  Energyem += de; 
-}
+inline void B4aEventAction::Addem(G4double de) {Energyem += de;}
 
-inline void B4aEventAction::AddScin(G4double de){
-  EnergyScin += de;
-}
+inline void B4aEventAction::AddScin(G4double de){EnergyScin += de;}
 
-inline void B4aEventAction::AddCher(G4double de){
-  EnergyCher += de;
-}
+inline void B4aEventAction::AddCher(G4double de){EnergyCher += de;}
 
-inline void B4aEventAction::AddCherenkov(){
-  NofCherenkovDetected = NofCherenkovDetected + 1;
-}
-
-/*inline void B4aEventAction::AddScintillation(){
-  NofScintillationDetected = NofScintillationDetected +1;
-}*/
-
-inline void B4aEventAction::Addenergy(G4double de){
-  EnergyTot += de;
-}
-
-/*inline void B4aEventAction::AddEnergyfibre(G4double de, G4int number){
-    Signalfibre[number] += de;
-}*/
-
-/*inline void B4aEventAction::AddSignalfibre(G4int number){
-    Signalfibre[number] = Signalfibre[number] + 1;
-}*/
+inline void B4aEventAction::Addenergy(G4double de){EnergyTot += de;}
                      
-//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
-
-
 #endif
 
     
--- DRC4pifibers/DRC4pigeometry/Dream/B4/B4a/src/B4aEventAction.cc
+++ DRC4pifibers/DRC4pigeometry/Dream/B4/B4a/src/B4aEventAction.cc
@@ -45,21 +45,16 @@
 
 #include "podio/ROOTWriter.h"
 
-
-//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
-
-B4aEventAction::B4aEventAction()
+B4aEventAction::B4aEventAction() //definition of B4aEvenctAction contructor
  : G4UserEventAction(),
-   Energyem(0.),
+   Energyem(0.),                 //data members set to 0.
    neutrinoleakage(0.),
    leakage(0.),
    EnergyScin(0.),
    EnergyCher(0.),
-   NofCherenkovDetected(0),
-   //NofScintillationDetected(0),
    EnergyTot(0.),
    PrimaryParticleEnergy(0.),
-   VectorSignalsR(0.),
+   VectorSignalsR(0.),          //vectors set to 0-size vector
    VectorSignalsL(0.),
    VectorSignalsCherR(0.),
    VectorSignalsCherL(0.),
@@ -68,7 +63,6 @@ B4aEventAction::B4aEventAction()
    VectorR_loop(0.),
    VectorL_loop(0.),
    Fiber_Hits{0.},
-   Tracking_Hits{0.},
    s_caloHits(0),
    c_caloHits(0),
    aux_infoHits(0)
@@ -83,98 +77,98 @@ B4aEventAction::~B4aEventAction()
 
 void B4aEventAction::BeginOfEventAction(const G4Event* /*event*/)
 {  
-	//Time_distribution event
-	std::ofstream TimeFile;
-	TimeFile.open("Time.txt", std::ios_base::app);
-	TimeFile<<"Event "<<G4EventManager::GetEventManager()->GetConstCurrentEvent()->GetEventID()<<" % % %"<<G4endl;
-	TimeFile.close();
+  //Time_distribution event
+  std::ofstream TimeFile;
+  TimeFile.open("Time.txt", std::ios_base::app);
+  TimeFile<<"Event "<<G4EventManager::GetEventManager()->GetConstCurrentEvent()->GetEventID()<<" % % %"<<G4endl;
+  TimeFile.close();
 	
   // initialisation per event
   Energyem = 0.;
   EnergyScin = 0.;
   EnergyCher = 0.;
-  NofCherenkovDetected = 0;
-  //NofScintillationDetected = 0;
   EnergyTot = 0.;
   neutrinoleakage = 0.;
   leakage = 0.;
+  PrimaryParticleEnergy = 0;  
   
   int fNbOfBarrel = 40;
   int fNbOfEndcap = 35;
   int fNbOfZRot = 36;
-  /*for(int i=0;i<64;i++){
-   Signalfibre[i]=0;
-  }*///only if you want to use SignalFibre[64]
 	
-	for (int i=0;i<1000000;i++)
-	Fiber_Hits[i]={0.};
+  for (int i=0;i<1000000;i++) Fiber_Hits[i]={0.};
 	
-	for (int i=0;i<200;i++)
-	Tracking_Hits[i]={0.};
+  for (int i=0;i<VectorR.size();i++){VectorR.at(i)=0.;}
 
-    for (int i=0;i<VectorR.size();i++){
-    VectorR.at(i)=0.;
-    }
-    for (int i=0;i<VectorL.size();i++){
-    VectorL.at(i)=0.;
-    }
+  for (int i=0;i<VectorL.size();i++){VectorL.at(i)=0.;}
 	
-	for (int i=0;i<VectorR_loop.size();i++){
-    VectorR_loop.at(i)=0.;
-    }
-    for (int i=0;i<VectorL_loop.size();i++){
-    VectorL_loop.at(i)=0.;
-    }
+  for (int i=0;i<VectorR_loop.size();i++){VectorR_loop.at(i)=0.;}
+
+  for (int i=0;i<VectorL_loop.size();i++){VectorL_loop.at(i)=0.;}
        
-    for (int i=0;i<VectorSignalsR.size();i++){
-    VectorSignalsR.at(i)=0.;
-    }
-    for (int i=0;i<VectorSignalsL.size();i++){
-    VectorSignalsL.at(i)=0.;
-    }
-    for (int i=0;i<VectorSignalsCherR.size();i++){
-    VectorSignalsCherR.at(i)=0.;
-    }
-    for (int i=0;i<VectorSignalsCherL.size();i++){
-    VectorSignalsCherL.at(i)=0.;
-    }
-    PrimaryParticleEnergy = 0;  
+  for (int i=0;i<VectorSignalsR.size();i++){VectorSignalsR.at(i)=0.;}
     
-    for(int i=0;i<=fNbOfZRot*(fNbOfBarrel+fNbOfEndcap);i++){
-        if(VectorR.size() <= fNbOfZRot*(fNbOfBarrel+fNbOfEndcap)){
-    VectorR.push_back(0.);}}
-    for(int i=0;i<=fNbOfZRot*(fNbOfBarrel+fNbOfEndcap);i++){
-        if(VectorL.size() <= fNbOfZRot*(fNbOfBarrel+fNbOfEndcap)){
-    VectorL.push_back(0.);}}
-    
-    for(int i=0;i<=fNbOfZRot*(fNbOfBarrel+fNbOfEndcap);i++){
-        if(VectorR_loop.size() <= fNbOfZRot*(fNbOfBarrel+fNbOfEndcap)){
-    VectorR_loop.push_back(0.);}}
-    for(int i=0;i<=fNbOfZRot*(fNbOfBarrel+fNbOfEndcap);i++){
-        if(VectorL_loop.size() <= fNbOfZRot*(fNbOfBarrel+fNbOfEndcap)){
-    VectorL_loop.push_back(0.);}}
+  for (int i=0;i<VectorSignalsL.size();i++){VectorSignalsL.at(i)=0.;}
 
-    for(int i=0;i<=fNbOfZRot*(fNbOfBarrel+fNbOfEndcap);i++){
-        if(VectorSignalsR.size() <= fNbOfZRot*(fNbOfBarrel+fNbOfEndcap)){
-    VectorSignalsR.push_back(0.);}}
-    for(int i=0;i<=fNbOfZRot*(fNbOfBarrel+fNbOfEndcap);i++){
-        if(VectorSignalsL.size() <= fNbOfZRot*(fNbOfBarrel+fNbOfEndcap)){
-    VectorSignalsL.push_back(0.);}}
-    //VectorSignals.at(i)=0;}
-    for(int k=0;k<=fNbOfZRot*(fNbOfBarrel+fNbOfEndcap);k++){
-        if(VectorSignalsCherR.size() <= fNbOfZRot*(fNbOfBarrel+fNbOfEndcap)){
-    VectorSignalsCherR.push_back(0.);}}
-    for(int k=0;k<=fNbOfZRot*(fNbOfBarrel+fNbOfEndcap);k++){
-        if(VectorSignalsCherL.size() <= fNbOfZRot*(fNbOfBarrel+fNbOfEndcap)){
-    VectorSignalsCherL.push_back(0.);}}
+  for (int i=0;i<VectorSignalsCherR.size();i++){VectorSignalsCherR.at(i)=0.;}
+
+  for (int i=0;i<VectorSignalsCherL.size();i++){VectorSignalsCherL.at(i)=0.;}
+    
+  for(int i=0;i<=fNbOfZRot*(fNbOfBarrel+fNbOfEndcap);i++){
+    if(VectorR.size() <= fNbOfZRot*(fNbOfBarrel+fNbOfEndcap)){
+      VectorR.push_back(0.);
+    }
+  }
+
+  for(int i=0;i<=fNbOfZRot*(fNbOfBarrel+fNbOfEndcap);i++){
+    if(VectorL.size() <= fNbOfZRot*(fNbOfBarrel+fNbOfEndcap)){
+      VectorL.push_back(0.);
+    }
+  }
+    
+  for(int i=0;i<=fNbOfZRot*(fNbOfBarrel+fNbOfEndcap);i++){
+    if(VectorR_loop.size() <= fNbOfZRot*(fNbOfBarrel+fNbOfEndcap)){
+      VectorR_loop.push_back(0.);
+    }
+  }
+    
+  for(int i=0;i<=fNbOfZRot*(fNbOfBarrel+fNbOfEndcap);i++){
+    if(VectorL_loop.size() <= fNbOfZRot*(fNbOfBarrel+fNbOfEndcap)){
+      VectorL_loop.push_back(0.);
+    }
+  }
+
+  for(int i=0;i<=fNbOfZRot*(fNbOfBarrel+fNbOfEndcap);i++){
+    if(VectorSignalsR.size() <= fNbOfZRot*(fNbOfBarrel+fNbOfEndcap)){
+      VectorSignalsR.push_back(0.);
+    }
+  }
+    
+  for(int i=0;i<=fNbOfZRot*(fNbOfBarrel+fNbOfEndcap);i++){
+    if(VectorSignalsL.size() <= fNbOfZRot*(fNbOfBarrel+fNbOfEndcap)){
+    VectorSignalsL.push_back(0.);
+    }
+  }
+
+  for(int k=0;k<=fNbOfZRot*(fNbOfBarrel+fNbOfEndcap);k++){
+    if(VectorSignalsCherR.size() <= fNbOfZRot*(fNbOfBarrel+fNbOfEndcap)){
+      VectorSignalsCherR.push_back(0.);
+    }
+  }
+  
+  for(int k=0;k<=fNbOfZRot*(fNbOfBarrel+fNbOfEndcap);k++){
+    if(VectorSignalsCherL.size() <= fNbOfZRot*(fNbOfBarrel+fNbOfEndcap)){
+      VectorSignalsCherL.push_back(0.);
+    }
+  }
+
 }
 
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 
 void B4aEventAction::EndOfEventAction(const G4Event* event)
 {
-  // Accumulate statistics
-  //
+  // Accumulate statistics over events
 
   // get analysis manager
   G4AnalysisManager* analysisManager = G4AnalysisManager::Instance();
@@ -182,57 +176,37 @@ void B4aEventAction::EndOfEventAction(const G4Event* event)
   podio::EventStore * l_store = l_podioManager->GetEvtStore();
   podio::ROOTWriter * l_writer = l_podioManager->GetWriter();
     
-	char namee[80];
 	
-	std::ofstream eventFile;
-	eventFile.open("Event.txt", std::ios_base::app);
-	/*G4cout<<"%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"<<std::endl;
-	G4cout<<"\t ID \t Energy(MeV) \t S/C \t Position \t slice \t tower"<<std::endl;
-	G4cout<<"%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"<<std::endl;*/
-	int v=0;
-	G4double E=0.;
-	if(G4EventManager::GetEventManager()->GetConstCurrentEvent()->GetEventID()==0)	eventFile<<"EvtID\tFiberID\tEt\tXt\tYt\tZt\tFlagt\tslicet\ttowert"<<std::endl;
-	while(Fiber_Hits[v].F_ID!=0){
+  // write fiber-by-fiber into file and store in podio Hits
+  std::ofstream eventFile;
+  eventFile.open("Event.txt", std::ios_base::app);
+  /*G4cout<<"%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"<<std::endl;
+  G4cout<<"\t ID \t Energy(MeV) \t S/C \t Position \t slice \t tower"<<std::endl;
+  G4cout<<"%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"<<std::endl;*/
+  int v=0;
+  G4double E=0.;
+  if(G4EventManager::GetEventManager()->GetConstCurrentEvent()->GetEventID()==0) eventFile<<"EvtID\tFiberID\tEt\tXt\tYt\tZt\tFlagt\tslicet\ttowert"<<std::endl;
+  while(Fiber_Hits[v].F_ID!=0){
+    E = E+Fiber_Hits[v].F_E;
 
-	  //G4cout<<Fiber_Hits[v].F_ID<<"\t"<<Fiber_Hits[v].F_E<<"\t"<<Fiber_Hits[v].F_Type<<"\t"<<Fiber_Hits[v].F_X<<" "<<Fiber_Hits[v].F_Y<<" "<<Fiber_Hits[v].F_Z<<std::endl;
-	  E = E+Fiber_Hits[v].F_E;
-	  eventFile<<G4EventManager::GetEventManager()->GetConstCurrentEvent()->GetEventID()<<"\t"<<std::fixed << std::setprecision(3) <<Fiber_Hits[v].F_ID<<"\t"<<Fiber_Hits[v].F_E<<"\t"<<Fiber_Hits[v].F_X<<"\t"<<Fiber_Hits[v].F_Y<<"\t"<<Fiber_Hits[v].F_Z<<"\t"<<Fiber_Hits[v].F_Type<<"\t"<<Fiber_Hits[v].F_slice<<"\t"<<Fiber_Hits[v].F_tower<<std::endl;
-	  if (Fiber_Hits[v].F_Type == 1){
-	    auto l_hit = s_caloHits->create();
-	    l_hit.setCellID((Fiber_Hits[v].F_ID));
-	    l_hit.setEnergy(Fiber_Hits[v].F_E);
-	    l_hit.setPosition({Fiber_Hits[v].F_X,Fiber_Hits[v].F_Y,Fiber_Hits[v].F_Z});
-	  } else if (Fiber_Hits[v].F_Type == 0){
-	    auto l_hit = c_caloHits->create();              
-            l_hit.setCellID((Fiber_Hits[v].F_ID));
-            l_hit.setEnergy(Fiber_Hits[v].F_E);
-            l_hit.setPosition({Fiber_Hits[v].F_X,Fiber_Hits[v].F_Y,Fiber_Hits[v].F_Z});
-	  }
-	  v++;
-	}
-	//eventFile<<"%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"<<std::endl;
-	eventFile.close();
+    eventFile<<G4EventManager::GetEventManager()->GetConstCurrentEvent()->GetEventID()<<"\t"<<std::fixed << std::setprecision(3) <<Fiber_Hits[v].F_ID<<"\t"<<Fiber_Hits[v].F_E<<"\t"<<Fiber_Hits[v].F_X<<"\t"<<Fiber_Hits[v].F_Y<<"\t"<<Fiber_Hits[v].F_Z<<"\t"<<Fiber_Hits[v].F_Type<<"\t"<<Fiber_Hits[v].F_slice<<"\t"<<Fiber_Hits[v].F_tower<<std::endl;
+
+    if (Fiber_Hits[v].F_Type == 1){
+      auto l_hit = s_caloHits->create();
+      l_hit.setCellID((Fiber_Hits[v].F_ID));
+      l_hit.setEnergy(Fiber_Hits[v].F_E);
+      l_hit.setPosition({Fiber_Hits[v].F_X,Fiber_Hits[v].F_Y,Fiber_Hits[v].F_Z});
+    } 
+    else if (Fiber_Hits[v].F_Type == 0){
+      auto l_hit = c_caloHits->create();              
+      l_hit.setCellID((Fiber_Hits[v].F_ID));
+      l_hit.setEnergy(Fiber_Hits[v].F_E);
+      l_hit.setPosition({Fiber_Hits[v].F_X,Fiber_Hits[v].F_Y,Fiber_Hits[v].F_Z});
+    }
+    v++;
+  }
+  eventFile.close();
 	
-	
-	std::ofstream eventFile1;
-	eventFile1.open("Event_Track.txt", std::ios_base::app);
-	if(G4EventManager::GetEventManager()->GetConstCurrentEvent()->GetEventID()==0)	eventFile1<<"EvtIDtrack\tTrackID\tXtrackt\tYtrackt\tZtrackt\tparticlesnamet"<<std::endl;
-	for (v=0;v<200;v++){
-		if(Tracking_Hits[v].T_ID!=0){
-		eventFile1<<G4EventManager::GetEventManager()->GetConstCurrentEvent()->GetEventID()<<"\t"<<Tracking_Hits[v].T_ID<<"\t"<<Tracking_Hits[v].T_X<<"\t"<<Tracking_Hits[v].T_Y<<"\t"<<Tracking_Hits[v].T_Z<<"\t"<<Tracking_Hits[v].T_Name<<std::endl;
-	}}
-	eventFile1.close();
-	//eventFile<<"%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"<<std::endl;
-	
-	/*G4cout<<"%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"<<std::endl;
-	G4cout<<"N fired: "<<v<<"\t tot E: "<<E<<"\t Lor: "<<NofCherenkovDetected<<std::endl;*/
-	
-	
-  // fill histograms
-  //analysisManager->FillH1(1, Energymodule);
-  //analysisManager->FillH1(2, TrackLmodule);
-  //analysisManager->FillH1(3, EnergyScin);
-  
   // fill ntuple event by event
   analysisManager->FillNtupleDColumn(0, Energyem);
   analysisManager->FillNtupleDColumn(1, EnergyScin);
@@ -243,7 +217,7 @@ void B4aEventAction::EndOfEventAction(const G4Event* event)
   analysisManager->FillNtupleSColumn(6, PrimaryParticleName);
   analysisManager->FillNtupleDColumn(7, neutrinoleakage);
   analysisManager->FillNtupleDColumn(8, leakage);
-  analysisManager->AddNtupleRow();//columns with vector are automatically filled with this function
+  analysisManager->AddNtupleRow();          //columns with vector are automatically filled with this function
 
   auto l_hit = aux_infoHits->create();
   l_hit.setCellID(0);
